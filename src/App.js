3dee6559c7f8f0f2f08ce11c18274032e49b46a3
run1102 <127601403+run1102@users.noreply.github.com>
2023-06-19 17:10:31 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie"
+import "./App.css";
+
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+    <section class="container">
+      {isLoading ? (
+      <div class="loader">
+      <span class="loader__text">Loading...</span>
+      </div>
+      ) : (
+        <div class="movies">
+          {movies.map(movie => (
+        <Movie
+         key={movie.id}
+          id={movie.id}
+          year={movie.year}
+          title={movie.title}
+          summary={movie.summary}
+          poster={movie.medium_cover_image}
+        />
+    ))}
+        </div>
+    )}
+    </section>
+    );
+  }
+}
+
+export default App;
