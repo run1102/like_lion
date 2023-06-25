480d11e0d44ac8b4a4cfc12a367488ed1338fa58
run1102 <127601403+run1102@users.noreply.github.com>
2023-06-25 12:32:31 +0900

    App.js

--- src_route/App.js
+++ src_route/App.js
@@ -1,5 +1,6 @@
 import Hi from "./Hi";
 import Bye from "./Bye";
+import Start from "./Start"
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom"
 
@@ -7,16 +8,10 @@ function App() {
   return (
     <div>
       <Routes>
+      <Route exact path="/" element={[< Start/>]} />
         <Route exact path="/hi" element={[<Hi />]} />
         <Route exact path="/bye" element={[<Bye/>]} />
       </Routes>
-
-        <div>
-        <Link exact to="/hi">
-          <button>start</button>
-        </Link>
-      </div>
-
       </div>
 
   )
