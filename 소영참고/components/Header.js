65747fc62f7ef0c078c1922f79bfc99b5ac81378
run1102 <127601403+run1102@users.noreply.github.com>
2023-06-26 18:47:20 +0900

    Header.js

--- 소영참고/components/Header.js
+++ 소영참고/components/Header.js
@@ -1,20 +1,23 @@
 import React from "react"
 import styles from "./Header.module.css"
+import { Routes, Route, Link } from "react-router-dom"
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <div>
                 <div >
-                    <p className={styles.logout}>LOG OUT</p>
+                    <Link to="/" className={styles.logout}>LOG OUT</Link>
                 </div>
                 
-                <div className={styles.main}>
-                    <h4>킹고오더</h4>
-                </div>
-                
+                <Link to="/buymenu">
+                    <div className={styles.main}>
+                        <h4>킹고오더</h4>
+                    </div>
+                </Link>
+
                 <div className={styles.navigation}>
-                    <p>명륜 율전</p>
+                    <p>명륜 / 율전</p>
                 </div>
             </div>
         </header>
